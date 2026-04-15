f79c60047d3dd3500f4d4cd3ea334bef5dfbd764
mayaraed <mayaraed3@gmail.com>
2026-04-15 23:03:11 +0200

    Update inventory.cpp

--- GEM_Game/inventory.cpp
+++ GEM_Game/inventory.cpp
@@ -1,1 +1,25 @@
+#include "Inventory.h"
+#include <iostream>
+using namespace std;
 
+void Inventory::addItem(Item* item) {
+    items.push_back(item);
+}
+
+void Inventory::showItems() {
+    for (int i = 0; i < items.size(); i++) {
+        cout << items[i]->getName() << endl;
+    }
+}
+
+int Inventory::countItem(string name) {
+    int count = 0;
+
+    for (int i = 0; i < items.size(); i++) {
+        if (items[i]->getName() == name) {
+            count++;
+        }
+    }
+
+    return count;
+}
