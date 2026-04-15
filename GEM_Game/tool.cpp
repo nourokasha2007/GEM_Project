2b1b8ce53c5e6d8aa35d55103179866cc4c1656a
mayaraed <mayaraed3@gmail.com>
2026-04-15 23:03:55 +0200

    Update tool.cpp

--- GEM_Game/tool.cpp
+++ GEM_Game/tool.cpp
@@ -1,1 +1,25 @@
+#include "Tool.h"
+#include <iostream>
+using namespace std;
 
+Tool::Tool() : Item() {
+    uses = 0;
+}
+
+Tool::Tool(string n, bool c, int u)
+    : Item(n, c, "Tool") {
+    uses = u;
+}
+
+void Tool::use() {
+    if (uses > 0) {
+        uses--;
+        cout << name << " used. Remaining uses: " << uses << endl;
+    } else {
+        cout << name << " has no uses left." << endl;
+    }
+}
+
+int Tool::getUses() {
+    return uses;
+}
