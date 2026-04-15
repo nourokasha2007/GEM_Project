3ace890536d7b158f7466a1a4f876d76cfd08615
mayaraed <mayaraed3@gmail.com>
2026-04-15 23:03:34 +0200

    Update tools.h

--- GEM_Game/tools.h
+++ GEM_Game/tools.h
@@ -1,1 +1,18 @@
+#ifndef TOOL_H
+#define TOOL_H
 
+#include "Item.h"
+
+class Tool : public Item {
+private:
+    int uses;
+
+public:
+    Tool();
+    Tool(string n, bool c, int u);
+
+    void use();
+    int getUses();
+};
+
+#endif
