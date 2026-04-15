c33f15a9ec93f4ad033e02dfc3b62bf8f61e87f1
mayaraed <mayaraed3@gmail.com>
2026-04-15 23:02:35 +0200

    Update inventory.h

--- GEM_Game/inventory.h
+++ GEM_Game/inventory.h
@@ -1,1 +1,18 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
 
+#include <vector>
+#include "Item.h"
+using namespace std;
+
+class Inventory {
+private:
+    vector<Item*> items;
+
+public:
+    void addItem(Item* item);
+    void showItems();
+    int countItem(string name);
+};
+
+#endif
