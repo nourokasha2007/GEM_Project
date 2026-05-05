0c063012fdcd3a01c33163dec33bf92cb717e9a3
Nour Okasha <nourokasha@aucegypt.edu>
2026-05-06 01:29:15 +0300

    Final correct version

--- GEM_Game/artifact.cpp
+++ GEM_Game/artifact.cpp
@@ -6,5 +6,3 @@ Artifact::Artifact() : Item() {}
 
 
 Artifact::Artifact(string n, bool c): Item(n,c, "Artifact") {}
-
-
--- GEM_Game/item.cpp
+++ GEM_Game/item.cpp
@@ -1,80 +1,38 @@
 
-#include <iostream>
+#include "item.h"
 #include <string>
-#include <vector>
-#include "item.h"
+
 using namespace std;
-
-Item::Item(){
-        name = "";
-        type = "";
-        collected = false;
-    }
-
-Item::Item(string n, bool c, string t) {
-        name = n;
-        type=t;
-        collected = c;
-    }
-
-    void Item::collect() {
-        collected = true;
-    }
-
-    string Item::getName() {
-        return name;
-    }
-
-    bool Item::isCollected() {
-        return collected;
-    }
-    string Item::getType() {
-        return type;
-        }
-
-
-
-
-
-
- 
-
-    
-    
-    
-    
-   
-#include "item.h"
-
-Item::Item() {
+Item::Item()
+{
     name = "";
     type = "";
     collected = false;
 }
-
-Item::Item(string n, bool c, string t) {
+Item::Item(string n, bool c, string t)
+{
     name = n;
     type = t;
     collected = c;
 }
-
-void Item::collect() {
+void Item::collect()
+{
     collected = true;
 }
-
-string Item::getName() {
+string Item::getName()
+{
     return name;
 }
-
-bool Item::isCollected() {
+bool Item::isCollected()
+{
     return collected;
 }
-
-string Item::getType() {
+string Item::getType()
+{
     return type;
 }
-
-void Item::clear() {
+void Item::clear()
+{
     name = "";
     type = "";
     collected = false;
