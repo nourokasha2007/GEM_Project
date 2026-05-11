55e2bf475c15ee986651d8a4b53e837ac78e0260
Nour Okasha <nourokasha@aucegypt.edu>
2026-05-11 19:13:22 +0300

    Latest code

--- GEM_Game/game.cpp
+++ GEM_Game/game.cpp
@@ -9,7 +9,7 @@ using namespace std;
 /* ================= CONSTRUCTOR ================= */
 
 Game::Game()
-    : player("Player", 100, 600)
+    : player("Player", 650, 810)
 {
     state = Gamestate::paused;
 
@@ -25,17 +25,15 @@ Game::Game()
 
     //================ COUNTS ================//
 
-    coinCount = 3;
+    coinCount = 2;
 
-    scrollCount =3;
+    scrollCount=2;
 
-    maskCount =  3;
+    maskCount = 2;
 
-    amuletCount =
-        3;
+    amuletCount =2;
 
-    timerCount =
-        3;
+    timerCount =2;
 }
 
 /* ================= DESTRUCTOR ================= */
@@ -96,19 +94,19 @@ void Game::restartGame()
     //================ RESET COUNTS ================//
 
     coinCount =
-        3;
+        2;
 
     scrollCount =
-        3;
+        2;
 
     maskCount =
-        3;
+        2;
 
     amuletCount =
-        3;
+        2;
 
     timerCount =
-        3;
+        2;
 }
 
 /* ================= EXIT ================= */
